9b13e6484538530a316b52b48de9b12d046bcae2
Masaki Ikeda <imasaki20@gmail.com>
2022-08-24 10:49:52 +0900

    Add TinySegmenter.

---
+++ TinySegmenterTests/TinySegmenterTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TinySegmenter.Tests
+{
+    [TestClass()]
+    public class TinySegmenterTests
+    {
+        [TestMethod()]
+        public void SegmentTest()
+        {
+            var tokens = TinySegmenter.Segment("私の名前は中野です");
+            Assert.AreEqual("私 | の | 名前 | は | 中野 | です", string.Join(" | ", tokens));
+        }
+    }
+}
